c43d787726d5d69e0e2788cea7c4db5661d19e51
Danni Yu <dyu@etsy.com>
2018-08-22 11:41:23 -0400

    add some js validations

--- src/webapp/static/index.js
+++ src/webapp/static/index.js
@@ -2,23 +2,35 @@ $( document ).ready(function() {
 
     $('button[type="submit"]').click(function() {
         var data = [];
+        var valid = true;
         $( 'input[type="checkbox"]').each( function() {
             data.push(this.id + "=" + this.checked);
         });
 
-        $( 'input[type="number"]' ).each(function() {
-            data.push(this.id + "=" + $(this).val());
+        $( 'input[type="text"], input[type="number"]' ).each(function() {
+            if (this.id === 'date' && !/[12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])/.test($(this).val())) {
+                alert("Invalid date format");
+                valid = false;
+                return false;
+            } else if (!$(this).val()) {
+                alert("Need to provide a value for " + $(this).prev().text());
+                valid = false;
+                return false;
+            } else {
+                data.push(this.id + "=" + $(this).val());
+            }
         });
 
-        $( 'input[type="text"]' ).each(function() {
-            data.push(this.id + "=" + $(this).val());
-        });
+        if (valid) {
+            var dataStr = '?' + data.join('&');
 
-        var dataStr = '?' + data.join('&');
-
-        $.ajax("/prediction" + dataStr)
-            .done(function(result) {
-                $('#prediction').text(result);
-            });
+            $.ajax("/prediction" + dataStr)
+                .done(function(result) {
+                    $('#prediction').text(result);
+                })
+                .fail(function() {
+                    alert("Request failed.")
+                });
+        }
     })
 });
